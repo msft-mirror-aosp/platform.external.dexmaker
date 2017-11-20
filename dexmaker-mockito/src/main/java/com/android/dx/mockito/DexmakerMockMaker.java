43b4c624c27e484d5e285533cf40ccf22980c6b6
Philip P. Moltmann <moltmann@google.com>
2017-11-20 09:38:54 -0800

    Update dexmaker to latest (master) state
    This adds dexmaker-mockito-inline support and dexmaker-mockito-*-tests.
    Inline mocking will allow mocking of final methods/classes.
    
    Bug: 63538681
    Test: cts-tradefed run cts-dev -m CtsMockingTestCases
    Change-Id: Id8b88639abe0a84642273eae43322df09068a782

--- dexmaker-mockito/src/main/java/com/android/dx/mockito/DexmakerMockMaker.java
+++ dexmaker-mockito/src/main/java/com/android/dx/mockito/DexmakerMockMaker.java
@@ -109,12 +109,15 @@ public final class DexmakerMockMaker implements MockMaker, StackTraceCleanerProv
         return new StackTraceCleaner() {
             @Override
             public boolean isIn(StackTraceElement candidate) {
+                String className = candidate.getClassName();
+
                 return defaultCleaner.isIn(candidate)
-                        && !candidate.getClassName().endsWith("_Proxy") // dexmaker class proxies
-                        && !candidate.getClassName().startsWith("$Proxy") // dalvik interface proxies
-                        && !candidate.getClassName().startsWith("java.lang.reflect.Proxy")
-                        && !candidate.getClassName().startsWith("com.google.dexmaker.mockito.")
-                        && !candidate.getClassName().startsWith("com.android.dx.mockito.");
+                        && !className.endsWith("_Proxy") // dexmaker class proxies
+                        && !className.startsWith("$Proxy") // dalvik interface proxies
+                        && !className.startsWith("java.lang.reflect.Proxy")
+                        && !(className.startsWith("com.android.dx.mockito.")
+                             // Do not clean unit tests
+                             && !className.startsWith("com.android.dx.mockito.tests"));
             }
         };
     }
